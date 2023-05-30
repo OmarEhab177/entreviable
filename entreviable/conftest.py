62c326d3aa25a23dff8dbdaa561cb7d55147a4df
Omar <omarehap17@gmail.com>
2023-05-30 18:20:07 +0300

    init commit

---
+++ entreviable/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from entreviable.users.models import User
+from entreviable.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
